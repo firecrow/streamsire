f63f11ab4005180c7b645666c69e977183305196
Fire Crow <fire@firecrow.com>
2014-12-08 20:34:29 -0500

    removing unused old version of region Signed-off-by: Fire Crow <fire@firecrow.com>

--- streamsire.js
+++ streamsire.js
@@ -237,122 +237,6 @@ if(!window.firecrow) window.firecrow = {};
         for(prop in from)
             to[prop] = from[prop];
     }
-    
-    var Overlay = {
-            _match_stage:0,
-            _START_MATCHING:3,
-            _MID_MATCHING:4,
-            _END_MATCHING:5,
-            init_region: function(start,mid_patterns,end)
-            {
-                this._shelf = '';
-                this.state = 0;
-                this.value = '';
-                this._validate_init_region(start, mid_patterns, end);
-                if(mid_patterns && mid_patterns.constructor == Array && mid_patterns.length > 0)
-                {
-                    var mid_parser = new ns.Parser();
-                    ns.Parser.apply(mid_parser, mid_patterns);
-                }
-                else
-                    var mid_parser = null; 
-                this._pattern = {'start':start,'mid':mid_parser,'end':end}
-            },
-            _validate_pattern: function(name, pattern)
-            {
-                if(!(pattern instanceof ns.PatternInterface)) 
-                    throw new Error(
-                        'RegionPatternOverlay.init: "' + name + '" must be intanceof "PatternInterface" or subclass, ');
-            }, 
-            _validate_init_region: function(start, mid_patterns, end)
-            {
-                this._validate_pattern('start',start);
-                this._validate_pattern('end',end);
-                if(mid_patterns)
-                    for(var i=0; i < mid_patterns.length; i++)
-                        this._validate_pattern('mid_patterns[' + i + ']', mid_patterns[i]);
-            },
-            increment: function(c)
-            {
-                switch(this._match_stage)
-                {
-                    case ns.PatternInterface.NO_MATCH:
-                    case Overlay._START_MATCHING:
-                        this._is_start_match(c); 
-                        break;
-                    case Overlay._MID_MATCHING:
-                    case Overlay._END_MATCHING:
-                        this._is_end_match(c); 
-                        break;
-                }
-            }, 
-            _is_start_match: function(c)
-            {
-                // result = [statuscode, content]
-                this._pattern.start.increment(c);
-                switch(this._pattern.start.state)
-                {
-                    case ns.PatternInterface.NO_MATCH:
-                        this._shelf += c;
-                        this._set(ns.PatternInterface.NO_MATCH, this._get_shelf());
-                        break;
-                    case ns.PatternInterface.MATCHING:
-                        this._match_stage = Overlay._START_MATCHING;
-                        this._shelf += c;
-                        this._set(ns.PatternInterface.MATCHING, '');
-                        break; 
-                    case ns.PatternInterface.MATCH:
-                        this._match_stage = Overlay._MID_MATCHING;
-                        this._shelf += c;
-                        this._set(ns.PatternInterface.MATCHING, '');
-                        break; 
-                }
-            },
-            _is_end_match: function(c)
-            {
-                // result = [statuscode, content]
-                this._pattern.end.increment(c); 
-                switch(this._pattern.end.state)
-                {
-                    case ns.PatternInterface.MATCHING:
-                        this._match_stage = Overlay._END_MATCHING
-                        this._shelf += c;
-                        this._set(ns.PatternInterface.MATCHING, '');
-                        break; 
-                    case  ns.PatternInterface.MATCH:
-                        this.reset()
-                        this._shelf += c; 
-                        this._set(ns.PatternInterface.MATCH, this._get_shelf());
-                        break; 
-                    default:
-                        this._is_mid_match(c);
-                        this._set(ns.PatternInterface.MATCHING, '');
-                        break;
-                }
-            }, 
-            _is_mid_match: function(c)
-            {
-                if(this._pattern.mid)
-                    this._pattern.mid.comparemanager.run(c);
-                    this._shelf += this._pattern.mid.comparemanager._round_val; 
-            }, 
-            reset: function()
-            {
-                // todo: see if this is important
-                this._match_stage = ns.PatternInterface.NO_MATCH;
-            },
-            toString: function()
-            {
-                return '[object RegionPattern]';
-            }
-        }
-
-    var RegionPattern = function(start, mid_patterns, end){ this.init_region(start, mid_patterns, end); }
-
-        RegionPattern.prototype = new ns.PatternInterface;
-        copyprops(RegionPattern.prototype, Overlay);
-    
-    copyprops(ns, {'RegionPatternOverlay':Overlay, 'RegionPattern':RegionPattern}); 
 
 })(window.firecrow); // pass namespace in here
 
@@ -362,7 +246,7 @@ if(!window.firecrow) window.firecrow = {};
 
 (function(ns){// tag pattern and tag region pattern 
 
-    if(!(ns.PatternInterface && ns.RegionPatternOverlay))
+    if(!ns.PatternInterface)
         throw new Error('tag_patterns: depends on "patterns", and "parser", no found in "ns"');
 
     function copyprops(to, from)
@@ -496,16 +380,7 @@ if(!window.firecrow) window.firecrow = {};
         }
 
 
-    var RegionTagPattern = function(name, start, mid_patterns, end)
-        {
-            this.init_tag(name);
-            this.init_region(start, mid_patterns, end);
-        } 
-        RegionTagPattern.prototype = new TagPatternInterface;
-        copyprops(RegionTagPattern.prototype, ns.RegionPatternOverlay);
-
     copyprops(ns, {'TagPatternInterface':TagPatternInterface, 'TagPattern':TagPattern, 
-        'RegionTagPattern':RegionTagPattern,
         'TagWordPattern':TagWordPattern}); 
 
 })(window.firecrow); // pass namespace in here 
